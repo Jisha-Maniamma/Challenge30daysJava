159b0540da09fa81b40f7b1b3b272881eb1efac1
Jisha <Jisha@admin-PC>
2019-07-01 15:07:24 +0900

    print star pattern

---
+++ 30DaysJavaCodingChallenge/src/Day6_PrintStars.java
@@ -0,0 +1,38 @@
+/*
+ * To print-
+ *  * * * * * * * * * * * * * * * * * * * * 1
+ *  * *   *   *   *   *   *   *   *   *   * 2
+ *  *   *     *     *     *     *     *     3
+ *  * *   *       *       *       *       * 4
+ *  *       *         *         *         * 5
+ *  * * *     *           *           *     6
+ *  *           *             *             7
+ *  * *   *       *               *         8
+ *  *   *           *                 *     9
+ *  * *     *         *                   * 10
+ *  *                   *                   11
+ *  * * * *   *           *                 12
+ *  *                       *               13
+ *  * *         *             *             14
+ *  *   *   *                   *           15
+ *  * *   *       *               *         16
+ *  *                               *       17
+ *  * * *     *     *                 *     18
+ *  *                                   *   19
+ *  * *   * *         *                   * 20
+ * 
+ */
+public class Day6_PrintStars {
+public static void main(String[] args) {
+	int n=Integer.parseInt("20");
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=n;j++)
+		{
+			if(j%i==0 || i%j==0)
+				System.out.print("*");
+			else 
+				System.out.print(" ");
+				}
+	System.out.println(i);}
+}
+}
