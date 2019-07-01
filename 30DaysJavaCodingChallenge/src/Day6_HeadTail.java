8781347cb00ba038789305ccce06acffed1ba173
Jisha <Jisha@admin-PC>
2019-07-01 11:30:16 +0900

    java Program

---
+++ 30DaysJavaCodingChallenge/src/Day6_HeadTail.java
@@ -0,0 +1,9 @@
+
+public class Day6_HeadTail {
+public static void main(String[] args) {
+	if(Math.random()>0.6)
+		System.out.println("Head");
+	else
+		System.out.println("Tail");
+}
+}
---
+++ 30DaysJavaCodingChallenge/src/Day6_RandomInt.java
@@ -0,0 +1,10 @@
+
+public class Day6_RandomInt {
+public static void main(String[] args) {
+	int n=Integer.parseInt("6");
+	double r=Math.random();
+
+	int a=(int) (n*r);
+	System.out.println(a);
+}
+}
